db6ec08694bbf5f21c8e7b0f68ec2cebda97e060
Gabo250 <ggaborr1@gmail.com>
2023-11-09 21:39:00 +0100

    SEO changes

--- src/component/home/Marketing.jsx
+++ src/component/home/Marketing.jsx
@@ -4,11 +4,11 @@ function Marketing() {
     return (
         <section className="relative py-11 pr-12 flex flex-row justify-end items-center gap-20 2xl:flex-col lg:pr-0 xsm:gap-y-20">
             <article className="relative w-[700px] mr-40 xl:mr-10 lg:!mr-0 2xl:w-1/2 xmd:!w-11/12">
-                <p className="text-justify leading-7">
+                <h2 className="text-justify leading-7">
                     Ha szép és minőségi munkára van szükséged, ne késlekedj lépj kapcsolatba velem.
                     Örömmel segítek megvalósítani otthonod vagy üzleted elképzeléseit.
                     Vedd fel velem a kapcsolatot, és kérd az akár ingyenes felmérést és árajánlatot!
-                </p>
+                </h2>
             </article>
             <div className="flex flex-row gap-20 lg:flex-wrap lg:justify-center xsm:flex-col">
                 {
@@ -20,7 +20,7 @@ function Marketing() {
                                                 before:h-[5px] before:w-[11rem]
                                                 after:absolute after:bg-white after:-bottom-[5px] after:left-[50%]
                                                 after:-translate-x-[50%] after:h-[5px] after:w-[11rem]">
-                                <h2 className="text-[1.25rem] font-bold text-lime-700">{ text.name }</h2>
+                                <h3 className="text-[1.25rem] font-bold text-lime-700">{ text.name }</h3>
                                 <p className="leading-7 text-justify">{ text.description }</p>
                             </article>
                         );
