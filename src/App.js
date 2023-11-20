80bb07f1bcfc416bc55a0d30cd49de2e3390f710
Gabo250 <ggaborr1@gmail.com>
2023-11-20 17:53:37 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import Gallery from "./component/gallery/Gallery";
 import ScrollTop from "./component/utility/ScrollTop";
 import NotFound from "./component/errorComp/NotFound";
 import Cookie from "./component/footer/Cookie";
-import RoomPainter from "./component/Services/RoomPaint";
-import Daub from "./component/Services/Daub";
-import PaperHanging from "./component/Services/PaperHanging";
-import PlasterBorading from "./component/Services/PlasterBoarding";
 
 function App() { 
 
@@ -21,11 +17,7 @@ function App() {
       <Header />
       <Routes>       
           <Route path="/" exact element={ <Home /> } />
-          <Route path="/szolgaltatasok" element={ <Services /> } /> 
-          <Route path="/szobafestes" element={ <RoomPainter /> } />
-          <Route path="/mazolas" element={ <Daub /> } />
-          <Route path="/tapetazas" element={ <PaperHanging /> } />
-          <Route path="/gipszkartonozas" element={ <PlasterBorading /> } />        
+          <Route path="/szolgaltatasok" element={ <Services /> } />         
           <Route path="/arak" element={ <Price /> } /> 
           <Route path="/munkak/*" element={ <Gallery /> } />          
           <Route path="/*" element={ <NotFound /> } />
